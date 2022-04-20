10615834f74713150f5458b89d642da3e8cd3eda
RKBethke <ryanbethke11@gmail.com>
2022-04-20 08:57:36 +0000

    theme: Enable italics

--- lua/colors/gruvbox-material.lua
+++ lua/colors/gruvbox-material.lua
@@ -5,6 +5,7 @@ function M.setup()
     vim.g.background = "dark"
     vim.g.gruvbox_material_background = "hard"
     vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_enable_italic = 1
     vim.g.gruvbox_material_sign_column_background = "none"
     vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
     vim.g.gruvbox_material_better_performance = 1
