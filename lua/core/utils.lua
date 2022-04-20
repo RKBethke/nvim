74a729853473d37c57a2a384bc14584ce63a0e33
RKBethke <ryanbethke11@gmail.com>
2022-04-20 08:59:02 +0000

    fix: Remove unnecessary status line hide command

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -115,37 +115,6 @@ M.close_buffer = function(force)
     end
 end
 
--- hide statusline
--- tables fetched from load_config function
-M.hide_statusline = function()
-    local hidden = {
-        "help",
-        "dashboard",
-        "NvimTree",
-        "terminal",
-    }
-    local shown = {}
-    local api = vim.api
-    local buftype = api.nvim_buf_get_option("%", "ft")
-
-    -- shown table from config has the highest priority
-    if vim.tbl_contains(shown, buftype) then
-        api.nvim_set_option("laststatus", 2)
-        return
-    end
-
-    if vim.tbl_contains(hidden, buftype) then
-        api.nvim_set_option("laststatus", 0)
-        return
-    end
-
-    api.nvim_set_option("laststatus", 2)
-end
-
-M.load_config = function()
-    return require("core.default_config")
-end
-
 M.map = function(mode, keys, command, opt)
     local options = { noremap = true, silent = true }
     if opt then
