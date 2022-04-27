0844a299a73be681f739235df6662bf90975e330
RKBethke <ryanbethke11@gmail.com>
2022-04-27 12:01:54 +0000

    feat: Use filetype.lua for faster startup

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 local ok, err = pcall(require, "core")
 
+-- experimental filetype.lua
+-- This will exclusively use filetype.lua for filetype matching
+vim.g.do_filetype_lua = 1;
+vim.g.did_load_filetypes = 0;
+
 if not ok then
     error("Error: " .. err)
 end
