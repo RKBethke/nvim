fbb02cd127f9043f28785bbc30edd9a72d8e2bcc
RKBethke <ryanbethke11@gmail.com>
2022-05-11 15:07:14 +0000

    chore: Remove unnecessary comments for null-ls

--- lua/plugins/configs/_null-ls.lua
+++ lua/plugins/configs/_null-ls.lua
@@ -3,7 +3,6 @@ local b = null_ls.builtins
 
 local sources = {
     -- b.formatting.prettierd.with { filetypes = { "html", "markdown", "css" } },
-    -- b.formatting.deno_fmt,
 
     -- Lua
     -- Only format if there is a stylua.toml file
@@ -12,13 +11,7 @@ local sources = {
             return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
         end,
     }),
-    -- b.formatting.stylua,
     b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
-
-    -- Shell
-    --
-    -- b.formatting.shfmt,
-    -- b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
 }
 
 local M = {}
