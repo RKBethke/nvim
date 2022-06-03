d67b059106e6bda94e6a6f1c51dd63ad151503a5
RKBethke <ryanbethke11@gmail.com>
2022-06-03 21:51:50 +0000

    feat(lsp): Add floating lsp rename

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -82,6 +82,64 @@ vim.notify = function(msg, log_level)
     end
 end
 
+-- credits to @Malace : https://www.reddit.com/r/neovim/comments/ql4iuj/rename_hover_including_window_title_and/
+   -- This is modified version of the above snippet
+   vim.lsp.buf.rename = {
+      float = function()
+         local currName = vim.fn.expand "<cword>" .. " "
+
+         local win = require("plenary.popup").create(currName, {
+            title = "Rename",
+            style = "minimal",
+            borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+            relative = "cursor",
+            borderhighlight = "RenamerBorder",
+            titlehighlight = "RenamerTitle",
+            focusable = true,
+            width = 25,
+            height = 1,
+            line = "cursor+2",
+            col = "cursor-1",
+         })
+
+         local map_opts = { noremap = true, silent = true }
+
+         vim.cmd "normal w"
+         vim.cmd "startinsert"
+
+         vim.api.nvim_buf_set_keymap(0, "i", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
+         vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
+
+         vim.api.nvim_buf_set_keymap(
+            0,
+            "i",
+            "<CR>",
+            "<cmd>stopinsert | lua vim.lsp.buf.rename.apply(" .. currName .. "," .. win .. ")<CR>",
+            map_opts
+         )
+
+         vim.api.nvim_buf_set_keymap(
+            0,
+            "n",
+            "<CR>",
+            "<cmd>stopinsert | lua vim.lsp.buf.rename.apply(" .. currName .. "," .. win .. ")<CR>",
+            map_opts
+         )
+      end,
+
+      apply = function(curr, win)
+         local newName = vim.trim(vim.fn.getline ".")
+         vim.api.nvim_win_close(win, true)
+
+         if #newName > 0 and newName ~= curr then
+            local params = vim.lsp.util.make_position_params()
+            params.newName = newName
+
+            vim.lsp.buf_request(0, "textDocument/rename", params)
+         end
+      end,
+   }
+
 local function on_attach(_, bufnr)
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
