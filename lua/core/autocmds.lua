2968a69719034e04f9f7901bc7b5fbec0d2cfa34
Ryan Bethke <ryanbethke11@gmail.com>
2022-06-08 11:51:49 -0500

    feat(ft): Add nix filetype support

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -34,13 +34,23 @@ vim.cmd([[ au TermOpen term://* setlocal nonumber norelativenumber | setfiletype
 --       vim.opt_local.softtabstop = 4
 --    end,
 -- })
-autocmd("Filetype", {
+autocmd("FileType", {
    pattern = "markdown",
    callback = function()
       vim.opt_local.expandtab = true
       vim.opt_local.tabstop = 4
       vim.opt_local.shiftwidth = 4
       vim.opt_local.softtabstop = 4
+   end,
+})
+
+autocmd("FileType", {
+   pattern = "nix",
+   callback = function()
+      vim.opt_local.expandtab = true
+      vim.opt_local.tabstop = 2
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.softtabstop = 2
    end,
 })
 -- 
