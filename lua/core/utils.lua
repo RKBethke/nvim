8e37420e82dd9ee4ac05ad569e8021b3e3cce1f2
Ryan Bethke <ryanbethke11@gmail.com>
2022-06-19 11:24:29 -0700

    fix(utils): Enable `close_buffer` to also close terminal

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,15 +1,18 @@
 _G.rb = {}
 
 local cmd = vim.cmd
+local api = vim.api
+local fn = vim.fn
 
 rb.close_buffer = function(force)
 	if vim.bo.buftype == "terminal" then
-		vim.api.nvim_win_hide(0)
-		return
+		force = force or #api.nvim_list_wins() < 2 and ":bd!"
+		local swap = force and #api.nvim_list_bufs() > 1 and ":bp | bd!" .. fn.bufnr()
+		return vim.cmd(swap or force or "hide")
 	end
 
-	local fileExists = vim.fn.filereadable(vim.fn.expand("%p"))
-	local modified = vim.api.nvim_buf_get_option(vim.fn.bufnr(), "modified")
+	local fileExists = fn.filereadable(fn.expand("%p"))
+	local modified = api.nvim_buf_get_option(fn.bufnr(), "modified")
 
 	-- if file doesnt exist & its modified
 	if fileExists == 0 and modified then
@@ -18,9 +21,8 @@ rb.close_buffer = function(force)
 	end
 
 	force = force or not vim.bo.buflisted or vim.bo.buftype == "nofile"
-
 	-- if not force, change to prev buf and then close current
-	local close_cmd = force and ":bd!" or ":bp | bd" .. vim.fn.bufnr()
+	local close_cmd = force and ":bd!" or ":bp | bd" .. fn.bufnr()
 	vim.cmd(close_cmd)
 end
 
