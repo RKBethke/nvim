3eec6086c5980708859ca4ecdd76acaee90b4288
Ryan Bethke <ryanbethke11@gmail.com>
2022-07-06 19:18:40 -0700

    fix(lspconfig): Do not capture focus for signatureHelp

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -60,6 +60,8 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 	border = "single",
+	focusable = false,
+	relative = "cursor",
 	max_width = max_width,
 	max_height = max_height,
 })
