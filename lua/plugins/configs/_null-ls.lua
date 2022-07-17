598b0f024732cc237f64017e3570f4ce866f07f3
Ryan Bethke <ryanbethke11@gmail.com>
2022-07-17 09:31:15 -0700

    feat(null-ls): Add shfmt and prettierd; remove luacheck

--- lua/plugins/configs/_null-ls.lua
+++ lua/plugins/configs/_null-ls.lua
@@ -2,17 +2,23 @@ local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local sources = {
-	-- b.formatting.prettierd.with { filetypes = { "html", "markdown", "css" } },
+	-- Web development
+	b.formatting.prettierd.with({ filetypes = { "html", "markdown", "css" } }),
 
 	-- Lua
-	-- Only format if there is a stylua.toml file
 	b.formatting.stylua.with({
+		-- Only format if there is a stylua.toml file
 		condition = function(utils)
 			return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
 		end,
 	}),
-	b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
-	b.code_actions.gitsigns, -- Enable gitsigns code actions
+
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+
+	-- Gitsigns
+	b.code_actions.gitsigns,
 }
 
 local M = {}
