15fcb79e322571f0cce64530a475514fb1e298d7
Ryan Bethke <ryanbethke11@gmail.com>
2022-07-17 09:27:01 -0700

    chore(autocmds): Re-organize and update to new API

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,5 +1,26 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Don't show any numbers inside terminals
+autocmd("TermOpen", {
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+		vim.opt_local.filetype = "terminal"
+	end,
+})
+
+-- Fix luasnip jumping erratically when not in insert mode
+autocmd("InsertLeave", {
+	callback = function()
+		if
+			require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+			and not require("luasnip").session.jump_active
+		then
+			require("luasnip").unlink_current()
+		end
+	end,
+})
+
 -- Uncomment this if you want to open nvim with a dir
 -- autocmd("BufEnter", {
 --    callback = function()
@@ -15,24 +36,9 @@ local autocmd = vim.api.nvim_create_autocmd
 --       vim.opt.relativenumber = false
 --    end,
 -- })
+
 -- autocmd("InsertLeave", {
 --    callback = function()
 --       vim.opt.relativenumber = true
 --    end,
 -- })
-
--- Don't show any numbers inside terminals
--- TODO: switch to use new autocmd api
-vim.cmd([[ au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal ]])
-
--- Fix luasnip jumping erratically when not in insert mode
-autocmd("InsertLeave", {
-	callback = function()
-		if
-			require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-			and not require("luasnip").session.jump_active
-		then
-			require("luasnip").unlink_current()
-		end
-	end,
-})
