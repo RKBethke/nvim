89d4d42717e4a92b460dc5ce32ab1af6071dd836
RKBethke <ryanbethke11@gmail.com>
2022-12-23 17:05:16 +0000

    feat(mappings): Center c-u and c-d

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -24,6 +24,8 @@ M.defaults = function()
 	map("n", "<Esc>", ":noh <CR>")
 
 	-- center cursor when moving (goto_definition)
+	map("n", "<C-u>", "<C-u>zz")
+	map("n","<C-d>", "<C-d>zz")
 	map("n", "n", "nzzzv")
 	map("n", "N", "Nzzzv")
 
