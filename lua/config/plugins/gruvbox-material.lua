7209794db6b5c1b712704dd4a24ee44e8a36d4e6
RKBethke <ryanbethke11@gmail.com>
2023-01-16 18:52:30 +0000

    chore(colorscheme): Split init and config

--- lua/config/plugins/gruvbox-material.lua
+++ lua/config/plugins/gruvbox-material.lua
@@ -1,7 +1,9 @@
 local M = {
 	"sainnhe/gruvbox-material",
 	priority = 1000,
-	lazy = false,
+	init = function()
+		vim.cmd([[colorscheme gruvbox-material]])
+	end,
 	config = function()
 		vim.g.gruvbox_material_background = "hard"
 		vim.g.gruvbox_material_enable_bold = 1
@@ -10,7 +12,6 @@ local M = {
 		vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 		vim.g.gruvbox_material_better_performance = 1
 		vim.g.gruvbox_material_ui_contrast = "high"
-		vim.cmd([[colorscheme gruvbox-material]])
 	end,
 }
 
