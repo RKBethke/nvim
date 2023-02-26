8816ab3fb6c9d01003d33a5c9cf11c487da4fc23
RKBethke <ryanbethke11@gmail.com>
2023-02-26 23:44:14 +0000

    fix(lsp_lines): Fix double-press enable requirement

--- lua/config/plugins/lsp/lsp_lines.lua
+++ lua/config/plugins/lsp/lsp_lines.lua
@@ -2,7 +2,17 @@ return {
 	url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 	dependencies = "neovim/nvim-lspconfig",
 	keys = {
-		{ "<leader>l", ":lua require('lsp_lines').toggle()<cr>" },
+		{
+			"<leader>l",
+			function()
+				if vim.g.lsp_lines_enabled then
+					require("lsp_lines").toggle()
+				else
+					vim.diagnostic.config({ virtual_lines = true })
+					vim.g.lsp_lines_enabled = true
+				end
+			end,
+		},
 	},
 	config = true,
 }
