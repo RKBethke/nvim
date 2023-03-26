30a86c5c7eaf54a0326acecb0cae5a35299b42bc
RKBethke <ryanbethke11@gmail.com>
2023-03-26 00:19:28 +0000

    chore(gruvbox-material): Use medium background

--- lua/config/plugins/gruvbox-material.lua
+++ lua/config/plugins/gruvbox-material.lua
@@ -1,17 +1,16 @@
 local M = {
 	"sainnhe/gruvbox-material",
 	priority = 1000,
-	init = function()
-		vim.cmd([[colorscheme gruvbox-material]])
-	end,
+	lazy = false,
 	config = function()
-		vim.g.gruvbox_material_background = "hard"
+		vim.g.gruvbox_material_background = "medium"
 		vim.g.gruvbox_material_enable_bold = 1
 		vim.g.gruvbox_material_enable_italic = 1
 		vim.g.gruvbox_material_sign_column_background = "none"
 		vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 		vim.g.gruvbox_material_better_performance = 1
-		vim.g.gruvbox_material_ui_contrast = "high"
+		vim.g.gruvbox_material_ui_contrast = "hard"
+		vim.cmd([[colorscheme gruvbox-material]])
 	end,
 }
 
