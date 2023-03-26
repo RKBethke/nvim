4f914418ef3633488825a437b3e576db0ca0d8df
RKBethke <ryanbethke11@gmail.com>
2023-03-26 06:09:51 +0000

    fix(leap): Remove unused "x" mappings.

--- lua/config/plugins/leap.lua
+++ lua/config/plugins/leap.lua
@@ -3,5 +3,8 @@ return {
 	event = "BufReadPost",
 	config = function()
 		require("leap").add_default_mappings()
+		-- Reset default behavior of 'x' and 'X'
+		vim.keymap.del({ "v", "o" }, "x")
+		vim.keymap.del({ "v", "o" }, "X")
 	end,
 }
