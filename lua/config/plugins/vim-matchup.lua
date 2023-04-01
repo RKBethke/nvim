8e874e9f7188da3fe89e37f7d34d1bf99fea7448
RKBethke <ryanbethke11@gmail.com>
2023-04-01 18:38:02 +0000

    feat(vim-fugitive): Add vim-fugitive

--- lua/config/plugins/vim-matchup.lua
+++ lua/config/plugins/vim-matchup.lua
@@ -1,10 +1,7 @@
-local M = {
+return {
 	"andymass/vim-matchup",
 	event = "BufReadPost",
+	config = function()
+		vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+	end,
 }
-
-function M.config()
-	vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
-end
-
-return M
