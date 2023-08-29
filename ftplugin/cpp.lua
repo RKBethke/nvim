d608079c36e9d96fffa695713ec0a21f9ec5096b
Ryan Bethke <ryanbethke11@gmail.com>
2023-08-29 13:15:36 -0700

    feat(ftplugin): Add ftplugin files for c, cpp, and text

---
+++ ftplugin/cpp.lua
@@ -0,0 +1,7 @@
+-- Spacing
+vim.bo.expandtab = false
+vim.bo.tabstop = 8
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+
+vim.bo.textwidth = 120
--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,2 +1,3 @@
 -- Enable spelling
 vim.wo.spell = true
+vim.bo.spelllang = "en,es"
