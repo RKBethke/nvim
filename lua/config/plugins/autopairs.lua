6120407c0c40eaab57455345b9ad97cf3e136d01
RKBethke <ryanbethke11@gmail.com>
2023-09-01 11:27:23 -0700

    fix(autopairs): Remove fast wrap disable setting

--- lua/config/plugins/autopairs.lua
+++ lua/config/plugins/autopairs.lua
@@ -3,12 +3,13 @@ return {
 	dependencies = {
 		"hrsh7th/nvim-cmp",
 	},
+	event = "InsertEnter",
 	config = function()
 		local autopairs = require("nvim-autopairs")
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
 
-		autopairs.setup({ fast_wrap = {} })
+		autopairs.setup({})
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
