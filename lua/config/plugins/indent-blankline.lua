4eeca005e827be54c2c2743b40e1cd939b78629b
RKBethke <ryanbethke11@gmail.com>
2023-10-01 19:33:05 -0700

    chore(indent-blankline): Update to V3 configuration

--- lua/config/plugins/indent-blankline.lua
+++ lua/config/plugins/indent-blankline.lua
@@ -1,24 +1,25 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	event = "BufReadPre",
 	opts = {
-		filetype_exclude = {
-			"help",
-			"terminal",
-			"dashboard",
-			"packer",
-			"lspinfo",
-			"TelescopePrompt",
-			"TelescopeResults",
-			"lsp-installer",
-			"NvimTree",
-			"",
+		exclude = {
+			filetypes = {
+				"help",
+				"terminal",
+				"dashboard",
+				"packer",
+				"lspinfo",
+				"TelescopePrompt",
+				"TelescopeResults",
+				"lsp-installer",
+				"NvimTree",
+				"",
+			},
+			buftypes = {
+				"terminal",
+				"nofile"
+			},
 		},
-		buftype_exclude = { "terminal", "nofile" },
-		show_trailing_blankline_indent = false,
-		show_first_indent_level = true,
-		show_end_of_line = true,
-		char = "▎",
-		space_char_blankline = " ",
 	},
 }
