d8fa8be52c31a73d0e539eced288a79d95772c7e
RKBethke <ryanbethke11@gmail.com>
2023-10-29 20:27:14 +0000

    feat(snippets): Add custom snippets directory with BQN snippets

--- lua/config/plugins/luasnip.lua
+++ lua/config/plugins/luasnip.lua
@@ -1,37 +1,34 @@
-local M = {
+return {
 	"L3MON4D3/LuaSnip",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets" })
 		end,
 	},
+	config = function()
+		local luasnip = require("luasnip")
+
+		luasnip.config.setup({
+			history = true,
+			updateevents = "TextChanged,TextChangedI",
+			enable_autosnippets = true,
+		})
+
+		-- Expansion Key:
+		-- Expand the current item or jump to the next item within the snippet.
+		vim.keymap.set({ "i", "s" }, "<M-j>", function()
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { silent = true })
+
+		-- Jump backwards key:
+		-- Moves to the previous item within the snippet
+		vim.keymap.set({ "i", "s" }, "<M-k>", function()
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { silent = true })
+	end,
 }
-
-function M.config()
-	local luasnip = require("luasnip")
-
-	luasnip.config.setup({
-		history = true,
-		updateevents = "TextChanged,TextChangedI",
-		enable_autosnippets = true,
-	})
-
-	-- Expansion Key:
-	-- Expand the current item or jump to the next item within the snippet.
-	vim.keymap.set({ "i", "s" }, "<M-j>", function()
-		if luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		end
-	end, { silent = true })
-
-	-- Jump backwards key:
-	-- Moves to the previous item within the snippet
-	vim.keymap.set({ "i", "s" }, "<M-k>", function()
-		if luasnip.jumpable(-1) then
-			luasnip.jump(-1)
-		end
-	end, { silent = true })
-end
-
-return M
