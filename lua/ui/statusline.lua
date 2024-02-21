ac7c7c98f23ee1301296e38beaf27fda6f328cc6
RKBethke <ryanbethke11@gmail.com>
2024-02-21 04:26:44 +0000

    feat(ui): Add macro recording mode to statusline

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -4,6 +4,7 @@ M.run = function()
 	return table.concat({
 		"%#StatusLine#",
 		M.file_info(),
+		M.extra_mode_status(),
 		"%=",
 		M.git(),
 		M.lsp_progress(),
@@ -119,6 +120,25 @@ function M.current_pos()
 	end
 
 	return "  " .. current_line .. ":" .. current_col .. "|" .. result
+end
+
+function M.extra_mode_status()
+	-- recording macros
+	local reg_recording = vim.fn.reg_recording()
+	if reg_recording ~= "" then
+		return "  @" .. reg_recording
+	end
+	-- executing macros
+	local reg_executing = vim.fn.reg_executing()
+	if reg_executing ~= "" then
+		return "  @" .. reg_executing
+	end
+	-- ix mode (<C-x> in insert mode to trigger different builtin completion sources)
+	local mode = vim.api.nvim_get_mode().mode
+	if mode == "ix" then
+		return " ^X: (^]^D^E^F^I^K^L^N^O^Ps^U^V^Y)"
+	end
+	return ""
 end
 
 return M
