2a357bd68a4abd7f26d50aa16d1a9ee778979818
RKBethke <ryanbethke11@gmail.com>
2024-05-18 23:58:05 +0000

    feat(oil): nvim-tree -> oil

---
+++ lua/config/plugins/oil.lua
@@ -0,0 +1,32 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"-",
+			function()
+				require("oil").open()
+			end,
+			mode = "n",
+			desc = "Open parent directory",
+		},
+		{
+			"<leader>-",
+			function()
+				require("oil").toggle_float()
+			end,
+			mode = "n",
+			desc = "Open parent directory",
+		},
+	},
+	opts = {
+		columns = { "icon" },
+		keymaps = {
+			["<C-h>"] = false,
+			["<M-h>"] = "actions.select_split",
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+}
