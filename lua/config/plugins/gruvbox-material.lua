17946c248ef84b491b9a36a598a664eabcd71c66
RKBethke <ryanbethke11@gmail.com>
2024-08-23 04:11:43 +0000

    chore(colorscheme): Disable italics and bold for gruvbox-material

--- lua/config/plugins/gruvbox-material.lua
+++ lua/config/plugins/gruvbox-material.lua
@@ -22,8 +22,8 @@ local M = {
 	},
 	config = function()
 		vim.g.gruvbox_material_background = "medium"
-		vim.g.gruvbox_material_enable_bold = 1
-		vim.g.gruvbox_material_enable_italic = 1
+		-- vim.g.gruvbox_material_enable_bold = 1
+		-- vim.g.gruvbox_material_enable_italic = 1
 		vim.g.gruvbox_material_sign_column_background = "none"
 		vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 		vim.g.gruvbox_material_better_performance = 1
