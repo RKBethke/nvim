02735b588381a3038d2931c43eac03f03ef6d620
RKBethke <ryanbethke11@gmail.com>
2024-09-04 03:32:33 +0000

    chore(ui): Add render-markdown.lua as well as new light color scheme: yui

---
+++ lua/config/plugins/yui.lua
@@ -0,0 +1,31 @@
+local M = {
+	"cideM/yui",
+	priority = 1100,
+	lazy = false,
+	config = function()
+		vim.cmd("set background=light")
+		vim.cmd.colorscheme("yui")
+	end,
+}
+
+M.colors = {
+	fg = "#ddc7a1",
+	bg = "#1d2021",
+	bg_statusline = "#282828",
+	accent = "#89b482",
+	lightbg = "#282828",
+	fgfaded = "#a89984",
+	grey = "#928374",
+	light_grey = "#a89984",
+	dark_grey = "#383432",
+	bright = "#fbf1c7",
+	red = "#ea6962",
+	green = "#a9b665",
+	blue = "#7daea3",
+	yellow = "#d8a657",
+	magenta = "#d3869b",
+	orange = "#e78a4e",
+	cyan = "#89b482",
+}
+
+return M
