9f7a73411299efd7b705406dbe7ca37218e27883
Ryan Bethke <ryanbethke11@gmail.com>
2025-01-12 16:18:13 -0800

    feat(blink): Use luasnip as blink snip source

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -1,28 +1,27 @@
 return {
 	"saghen/blink.cmp",
 	lazy = false, -- lazy loading handled internally
-	dependencies = "rafamadriz/friendly-snippets",
-	version = "v0.*",
+	dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+	version = "*",
 	opts = {
 		keymap = { preset = "default" },
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
+		snippets = { preset = "luasnip" },
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
-		-- experimental signature help support
+		-- Experimental signature help support.
 		signature = { enabled = true },
 		completion = {
 			accept = {
-				-- experimental auto-brackets support
-				auto_brackets = {
-					enabled = true,
-				},
+				auto_brackets = { enabled = true },
 			},
 			menu = {
 				draw = {
+					-- Use treesitter to highlight the label text for the given list of sources.
 					treesitter = { "lsp" },
 				},
 			},
