90037bb03d942520343486d2f92c0009473a5e0d
Ryan Bethke <ryanbethke11@gmail.com>
2025-06-01 18:05:11 -0700

    feat(no-neck-pain): s/zen-mode/no-neck-pain/g
    To address colorscheme flickering issues with zen-mode, switch to
    the no-neck-pain plugin for centering buffers.

---
+++ lua/config/plugins/no-neck-pain.lua
@@ -0,0 +1,6 @@
+return {
+	"shortcuts/no-neck-pain.nvim",
+	version = "*",
+	cmd = "NoNeckPain",
+	keys = { { "<leader>z", "<cmd>NoNeckPain<cr>", desc = "Zen Mode" } },
+}
--- lua/config/plugins/zen-mode.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"folke/zen-mode.nvim",
-	cmd = "ZenMode",
-	opts = {},
-	keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-}
