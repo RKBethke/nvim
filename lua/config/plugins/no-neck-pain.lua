26d2e36437d1d57af7203fe82f04430746c86de8
Ryan Bethke <ryanbethke11@gmail.com>
2025-06-22 21:34:12 -0700

    fix(no-neck-pain): Tweak width to consider borders

--- lua/config/plugins/no-neck-pain.lua
+++ lua/config/plugins/no-neck-pain.lua
@@ -1,12 +1,15 @@
 local function get_win_width()
 	local wid = vim.api.nvim_get_current_win()
 	local wininfo = vim.fn.getwininfo(wid)[1]
-	return vim.bo.textwidth + wininfo.textoff
+	local win_width = vim.bo.textwidth + wininfo.textoff + 2
+	vim.notify("textwidth=" .. vim.bo.textwidth, vim.log.levels.INFO)
+	vim.notify("textoff=" .. wininfo.textoff, vim.log.levels.INFO)
+	vim.notify("win_width=" .. win_width, vim.log.levels.INFO)
+	return win_width
 end
 
 return {
 	"shortcuts/no-neck-pain.nvim",
-	version = "*",
 	cmd = "NoNeckPain",
 	keys = { { "<leader>z", "<cmd>NoNeckPain<cr>", desc = "Zen Mode" } },
 	config = function()
