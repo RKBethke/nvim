e96fae99203ac3c41305259af3ec563b64c2ffbc
Ryan Bethke <ryanbethke11@gmail.com>
2025-08-05 21:47:59 -0700

    feat(fugitive): Add keybinding for copying git URL of current line

--- lua/config/plugins/vim-fugitive.lua
+++ lua/config/plugins/vim-fugitive.lua
@@ -3,6 +3,7 @@ return {
 	keys = {
 		{ "<leader>gs", "<cmd>G<cr>", desc = "status" },
 		{ "<leader>gd", "<cmd>Gvdiffsplit!<cr>", desc = "vdiffsplit" },
+		{ "<leader>gy", "<cmd>GBrowse!<cr>", desc = "Copy URL to clipboard" },
 		-- TODO: Map only for diff mode (vim.wo.diff)
 		{ "dgh", "<cmd>diffget //2<CR>", desc = "diffget left" },
 		{ "dgl", "<cmd>diffget //3<CR>", desc = "diffget right" },
