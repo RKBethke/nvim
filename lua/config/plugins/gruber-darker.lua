fa6b105600237d0b294ccd9bb283765bf8bf611e
Ryan Bethke <ryanbethke11@gmail.com>
2026-02-10 11:59:03 -0800

    chore(colorscheme): Remove unused color palette exports

--- lua/config/plugins/gruber-darker.lua
+++ lua/config/plugins/gruber-darker.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
 	"blazkowolf/gruber-darker.nvim",
 	lazy = true,
 	priority = 1000,
@@ -17,31 +17,3 @@ local M = {
 		})
 	end,
 }
-
-M.colors = {
-	none = "NONE",
-	fg = "#e4e4e4",
-	["fg+1"] = "#f4f4ff",
-	["fg+2"] = "#f5f5f5",
-	white = "#ffffff",
-	black = "#000000",
-	["bg-1"] = "#101010",
-	bg = "#181818",
-	["bg+1"] = "#282828",
-	["bg+2"] = "#453d41",
-	["bg+3"] = "#484848",
-	["bg+4"] = "#52494e",
-	["red-1"] = "#c73c3f",
-	red = "#f43841",
-	["red+1"] = "#ff4f58",
-	green = "#73d936",
-	yellow = "#ffdd33",
-	brown = "#cc8c3c",
-	quartz = "#95a99f",
-	["niagara-2"] = "#303540",
-	["niagara-1"] = "#565f73",
-	niagara = "#96a6c8",
-	wisteria = "#9e95c7",
-}
-
-return M
